9a6b393ba87a76236846b2ffa2fc37875f92cb6e
Atsushi-Kuwabara <48851734+Atsushi-Kuwabara@users.noreply.github.com>
2020-02-03 18:00:49 +0900

    メッセージの自動更新機能実装
    # WHAT
    - メッセージ自動更新機能の実装
    # WHY
    - チャットアプリには必須機能であるため

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,9 +1,27 @@
 $(function(){
 
   function buildHTML(message){
-    if (message.image) {
+    if (message.content && message.image) {
       var html = 
-        `<div class="main-chat__messages__message">
+        `<div class="main-chat__messages__message" data-message-id=${message.id}>
+          <div class="main-chat__messages__message__upper-info">
+            <div class="main-chat__messages__message__upper-info__username">
+              ${message.user_name}
+            </div>
+            <div class="main-chat__messages__message__upper-info__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="main-chat__messages__message__lower">
+            <p class="main-chat__messages__message__lower__text">
+              ${message.content}
+            </p>
+            <img src="${message.image}" class="main-chat__messages__message__lower__image">
+          </div>
+        </div>`
+    } else if(message.content){
+      var html = 
+        `<div class="main-chat__messages__message" data-message-id=${message.id}>
           <div class="main-chat__messages__message__upper-info">
             <div class="main-chat__messages__message__upper-info__username">
               ${message.user_name}
@@ -17,12 +35,10 @@ $(function(){
               ${message.content}
             </p>
           </div>
-          <img src=${message.image} >
         </div>`
-      return html;
-    } else {
+    } else if(message.image) {
       var html = 
-        `<div class="main-chat__messages__message">
+        `<div class="main-chat__messages__message" data-message-id=${message.id}>
           <div class="main-chat__messages__message__upper-info">
             <div class="main-chat__messages__message__upper-info__username">
               ${message.user_name}
@@ -32,14 +48,12 @@ $(function(){
             </div>
           </div>
           <div class="main-chat__messages__message__lower">
-            <p class="main-chat__messages__message__lower__text">
-              ${message.content}
-            </p>
+            <img src=${message.image} class="main-chat__messages__message__lower__image">
           </div>
         </div>`
-      return html;
     };
-  }
+    return html;
+  };
 
   $('#new_message').on('submit', function(e){
     e.preventDefault();
@@ -65,4 +79,30 @@ $(function(){
       alert('メッセージ送信に失敗しました')
     })
   });
+
+  var reloadMessages = function(){
+    last_message_id = $('.main-chat__messages__message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages){
+      if (messages.length !== 0){
+        var insertHTML = '';
+        $.each(messages, function(i, message){
+          insertHTML += buildHTML(message)
+        });
+        $('.main-chat__messages').append(insertHTML);
+        $('.main-chat__messages').animate({ scrollTop: $('.main-chat__messages')[0].scrollHeight});
+      }
+    })
+    .fail(function(){
+      console.log('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)){
+    setInterval(reloadMessages, 7000);
+  }
 });
